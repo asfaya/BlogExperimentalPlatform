8a965f775b962424ec04f01832a3b6f364836dd8
Andrés Faya <asfaya@hotmail.com>
2018-08-27 19:56:00 -0300

    Creating initial version of the entities.

---
+++ BlogExperimentalPlatform.Business/Entities/Blog.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogExperimentalPlatform.Business.Entities
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Blog : Entity
+    {
+        public string Name { get; set; }
+
+        public User Owner { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public ICollection<BlogEntry> Entries { get; set; }
+    }
+}
---
+++ BlogExperimentalPlatform.Business/Entities/BlogEntry.cs
@@ -0,0 +1,22 @@
+﻿namespace BlogExperimentalPlatform.Business.Entities
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BlogEntry : Entity
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public Blog Blog { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public BlogEntryStatus Status { get; set; }
+
+        public ICollection<BlogEntryUpdate> EntryUpdates { get; set; }
+    }
+}
