52a74a2dc0cbe2a6b011e890f7b47be4512e60fd
Andrés Faya <asfaya@hotmail.com>
2018-08-27 19:48:28 -0300

    Initial commit of the solution

---
+++ BlogExperimentalPlatform.Business.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace BlogExperimentalPlatform.Business.Test
+{
+    using System;
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ BlogExperimentalPlatform.Web/Pages/Error.cshtml.cs
@@ -0,0 +1,19 @@
+namespace BlogExperimentalPlatform.Web.Pages
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class Error : PageModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
