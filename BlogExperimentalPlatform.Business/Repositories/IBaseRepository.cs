b602b8de2ac928ab97382c97274c2b16680ec8b1
Andrés Faya <asfaya@hotmail.com>
2018-08-27 20:15:17 -0300

    Creating interface for base repo.
    Creating abstract class for base repo
    Creating Context

---
+++ BlogExperimentalPlatform.Business/Repositories/IBaseRepository.cs
@@ -0,0 +1,34 @@
+﻿namespace BlogExperimentalPlatform.Business.Repositories
+{
+    using BlogExperimentalPlatform.Business.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Base repository
+    /// It's aim is to provide the methods that all repos will have to provide.
+    /// Particular methods for different repositories will be defined on their own 
+    /// interface
+    /// </summary>
+    /// <typeparam name="T">Entity</typeparam>
+    public interface IBaseRepository<T> where T : Entity
+    {
+        Task<ICollection<T>> GetAllAsync();
+
+        Task<ICollection<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties);
+
+        Task<ICollection<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate);
+
+        Task<ICollection<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<T> GetSingleAsync(int id);
+
+        Task<T> GetSingleAsync(int id, params Expression<Func<T, object>>[] includeProperties);
+
+        Task<T> AddOrUpdateAsync(T entity);
+
+        Task DeleteAsync(int id);
+    }
+}
---
+++ BlogExperimentalPlatform.Data/BlogDbContext.cs
@@ -0,0 +1,29 @@
+﻿namespace BlogExperimentalPlatform.Data
+{
+    using BlogExperimentalPlatform.Business.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+
+    public class BlogDbContext : DbContext
+    {
+        #region Constructor
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options) { }
+        #endregion
+
+        #region Properties
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Blog> Blogs { get; set; }
+
+        public DbSet<BlogEntry> BlogEntries { get; set; }
+
+        public DbSet<BlogEntryUpdate> BlogEntryUpdates { get; set; }
+        #endregion
+
+        #region Methods
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+        #endregion
+    }
+}
