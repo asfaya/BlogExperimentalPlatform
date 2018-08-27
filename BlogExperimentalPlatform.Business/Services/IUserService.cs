3fac9754d1e76b22e9461ca3181dc54933541e50
Andrés Faya <asfaya@hotmail.com>
2018-08-27 20:38:22 -0300

    Adding interface and class for base service
    Adding interfaces for other services

---
+++ BlogExperimentalPlatform.Business/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogExperimentalPlatform.Business.Services
+{
+    using BlogExperimentalPlatform.Business.Entities;
+
+    /// <summary>
+    /// Service interface for Users
+    /// </summary>
+    public interface IUserService : IBaseService<User>
+    {
+    }
+}
