fa75245f9e8bd478bb7ba8efc70a79897a3fba24
Andrés Faya <asfaya@hotmail.com>
2018-08-27 20:41:39 -0300

    Adding implementations for service interfaces

---
+++ BlogExperimentalPlatform.Business/ServiceImplementations/BlogService.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogExperimentalPlatform.Business.ServiceImplementations
+{
+    using BlogExperimentalPlatform.Business.Entities;
+    using BlogExperimentalPlatform.Business.Repositories;
+    using BlogExperimentalPlatform.Business.Services;
+
+    public class BlogService : BaseService<Blog>, IBlogService
+    {
+        #region Constructor
+        public BlogService(IBlogRepository repository)
+            : base(repository)
+        {
+        }
+        #endregion
+    }
+}
