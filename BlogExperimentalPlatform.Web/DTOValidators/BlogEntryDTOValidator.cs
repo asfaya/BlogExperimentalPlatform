c63ae3fb7e00f3773c7df4c2bf49e9a1f21c8b95
Andrés Faya <asfaya@hotmail.com>
2018-08-28 19:30:39 -0300

    Adding FluentValidation
    Adding first validators

---
+++ BlogExperimentalPlatform.Web/DTOValidators/BlogEntryDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace BlogExperimentalPlatform.Web.DTOValidators
+{
+    using BlogExperimentalPlatform.Web.DTOs;
+    using FluentValidation;
+
+    public class BlogEntryDTOValidator : AbstractValidator<BlogEntryDTO>
+    {
+        public BlogEntryDTOValidator()
+            : base()
+        {
+            RuleSet("BlogEntryAddOrUpdate", () =>
+            {
+                RuleFor(b => b.Title).NotEmpty().WithMessage("Blog entry title is mandatory.");
+                RuleFor(b => b.Content).NotEmpty().WithMessage("Blog entry content is mandatory");
+                RuleFor(b => b.Status).NotNull().WithMessage("Blog entry status is mandatory");
+                RuleFor(b => b.Blog).NotNull().WithMessage("Blog entry must be created for a particular blog");
+            });
+        }
+    }
+}
