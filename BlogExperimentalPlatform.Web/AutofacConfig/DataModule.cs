49421499904ac3b5953af451855c683659fd84bb
Andrés Faya <asfaya@hotmail.com>
2018-08-28 18:39:49 -0300

    Adding DI with Autofac

---
+++ BlogExperimentalPlatform.Web/AutofacConfig/DataModule.cs
@@ -0,0 +1,22 @@
+﻿namespace BlogExperimentalPlatform.Web.AutofacConfig
+{
+    using System.Linq;
+    using System.Reflection;
+    using Autofac;
+    using BlogExperimentalPlatform.Data;
+    using BlogExperimentalPlatform.Data.Repositories;
+
+    public class DataModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BlogDbContext>().As<BlogDbContext>().InstancePerLifetimeScope();
+
+            builder
+                .RegisterAssemblyTypes(typeof(BlogRepository).GetTypeInfo().Assembly)
+                .Where(c => c.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
