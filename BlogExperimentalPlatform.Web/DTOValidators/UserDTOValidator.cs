d01bcbab61cc73e406831e16f2db72001431d25c
Andrés Faya <asfaya@hotmail.com>
2018-08-28 19:39:49 -0300

    Add UserDTOValidator
    Not adding BlogEntryUpdateDTO since it's created on the server. User is never going to input any manually

---
+++ BlogExperimentalPlatform.Web/DTOValidators/UserDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace BlogExperimentalPlatform.Web.DTOValidators
+{
+    using BlogExperimentalPlatform.Web.DTOs;
+    using FluentValidation;
+
+    public class UserDTOValidator : AbstractValidator<UserDTO>
+    {
+        public UserDTOValidator()
+            : base()
+        {
+            RuleSet("UserAddOrUpdate", () =>
+            {
+                RuleFor(u => u.FullName).NotEmpty().WithMessage("User full name is mandatory.");
+                RuleFor(u => u.UserName).NotEmpty().WithMessage("Username is mandatory.");
+            });
+        }
+    }
+}
