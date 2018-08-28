a3c2a4e3e0d50065894688a1258f1f9b7bc25e34
Andrés Faya <asfaya@hotmail.com>
2018-08-28 18:27:00 -0300

    Correcting visibility

--- BlogExperimentalPlatform.Data/Repositories/BaseRepository.cs
+++ BlogExperimentalPlatform.Data/Repositories/BaseRepository.cs
@@ -13,24 +13,24 @@
         where T : Entity
     {
         #region Members
-        private readonly BlogDbContext ctx;
+        private readonly BlogDbContext blogContext;
         #endregion
 
         #region Constructor
         public BaseRepository(BlogDbContext ctx)
         {
-            this.ctx = ctx ?? throw new ArgumentNullException("Error on BlogDbContext DI");
+            this.blogContext = ctx ?? throw new ArgumentNullException("Error on BlogDbContext DI");
         }
         #endregion
 
         #region Properties
-        public BlogDbContext Ctx => ctx;
+        protected BlogDbContext BlogContext => blogContext;
         #endregion
 
         #region IBaseRepository Methods
         public virtual async Task<ICollection<T>> GetAllAsync()
         {
-            IQueryable<T> query = Ctx.Set<T>()
+            IQueryable<T> query = BlogContext.Set<T>()
                 .AsNoTracking()
                 .Where(t => t.Deleted == false);
 
@@ -39,7 +39,7 @@
 
         public virtual async Task<ICollection<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
         {
-            IQueryable<T> query = Ctx.Set<T>()
+            IQueryable<T> query = BlogContext.Set<T>()
                 .AsNoTracking()
                 .Where(t => t.Deleted == false);
 
@@ -51,7 +51,7 @@
 
         public virtual async Task<ICollection<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate)
         {
-            IQueryable<T> query = Ctx.Set<T>()
+            IQueryable<T> query = BlogContext.Set<T>()
                 .AsNoTracking()
                 .Where(t => t.Deleted == false)
                 .Where(predicate);
@@ -61,7 +61,7 @@
 
         public virtual async Task<ICollection<T>> GetFilteredAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties)
         {
-            IQueryable<T> query = Ctx.Set<T>()
+            IQueryable<T> query = BlogContext.Set<T>()
                 .AsNoTracking()
                 .Where(t => t.Deleted == false)
                 .Where(predicate);
@@ -74,7 +74,7 @@
 
         public virtual async Task<T> GetSingleAsync(int id)
         {
-            return await Ctx.Set<T>()
+            return await BlogContext.Set<T>()
                 .Where(t => t.Deleted == false)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(e => e.Id == id);
@@ -82,7 +82,7 @@
 
         public virtual async Task<T> GetSingleAsync(int id, params Expression<Func<T, object>>[] includeProperties)
         {
-            var query = Ctx.Set<T>().AsNoTracking()
+            var query = BlogContext.Set<T>().AsNoTracking()
                 .Where(t => t.Id == id && t.Deleted == false);
             foreach (var includeProperty in includeProperties)
                 query = query.Include(includeProperty);
@@ -94,15 +94,15 @@
         {
             // Not for updating tree graphs
             // Need to override for that
-            var existingItem = await Ctx.Set<T>().FindAsync(entity.Id);
+            var existingItem = await BlogContext.Set<T>().FindAsync(entity.Id);
             if (existingItem == null)
-                await Ctx.Set<T>().AddAsync(entity);
+                await BlogContext.Set<T>().AddAsync(entity);
             else
-                Ctx.Entry(existingItem).CurrentValues.SetValues(entity);
+                BlogContext.Entry(existingItem).CurrentValues.SetValues(entity);
 
-            await Ctx.SaveChangesAsync();
+            await BlogContext.SaveChangesAsync();
 
-            return await Ctx.Set<T>().FindAsync(entity.Id);
+            return await BlogContext.Set<T>().FindAsync(entity.Id);
         }
 
         public virtual async Task DeleteAsync(int id)
@@ -110,7 +110,7 @@
             // Soft delete implementation
             var entity = await GetSingleAsync(id);
             entity.Deleted = true;
-            await Ctx.SaveChangesAsync();
+            await BlogContext.SaveChangesAsync();
         }
         #endregion
     }
