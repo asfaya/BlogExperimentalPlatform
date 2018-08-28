2b27c04c2f17c49844add191a99e0f3522bd87e5
asfaya <asfaya@hotmail.com>
2018-08-28 19:44:33 -0300

    20180828 Add fluent validation (#6)
    * Adding FluentValidation
    
    Adding first validators
    
    * Add UserDTOValidator
    
    Not adding BlogEntryUpdateDTO since it's created on the server. User is never going to input any manually

---
+++ BlogExperimentalPlatform.Web/DTOValidators/BlogDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace BlogExperimentalPlatform.Web.DTOValidators
+{
+    using BlogExperimentalPlatform.Web.DTOs;
+    using FluentValidation;
+
+    public class BlogDTOValidator : AbstractValidator<BlogDTO>
+    {
+        public BlogDTOValidator()
+            : base()
+        {
+            RuleSet("BlogAddOrUpdate", () =>
+            {
+                RuleFor(b => b.Name).NotEmpty().WithMessage("Blog name is mandatory.");
+                RuleFor(b => b.Owner).NotNull().WithMessage("Blog needs to have an owner");
+            });
+        }
+    }
+}
